f3421fa573e678f107eee05de8e2a54b197c9b48
Prabhat Kumar <prabhatsah349@gmail.com>
2025-01-15 00:49:36 +0530

    Database changed from mongo to mysql

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dorenv from "dotenv";
-import databaseConnection from "./utils/database.js";
+//import databaseConnection from "./utils/database.js";
 import cookieParser from "cookie-parser";
-import userRoute from "./routes/userRoute.js";
+import userRoute from "./src/routes/userRoute.js";
 import cors from "cors";
 
-databaseConnection();
+//databaseConnection();
 
 dorenv.config({
     path: ".env"
--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/userModel.js";
+import { User } from "../../models/userModel.js";
 import bcryptjs from "bcryptjs";
 
 export const Login = async (req, res) => {
